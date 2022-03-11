a97a314d7d220ea5ded67e3c012e939701c4c3c1
Luan <santos.pereira.luan@gmail.com>
2022-03-10 22:55:31 -0300

    WebSocket

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Client } from '@stomp/stompjs';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const SOCKET_URL = 'ws://localhost:8080/ws-messages';
+
+class App_Stomp extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      messages: 'You server message here.',
+    };
+  };
+
+  binArrayToJson(binArray){
+    var str = "";
+    for (var i = 0; i < binArray.length; i++) {
+      str += String.fromCharCode(parseInt(binArray[i]));
+    }
+    return JSON.parse(str)
+  }
+
+  componentDidMount() {
+    let currentComponent = this;
+    let onConnected = () => {
+      console.log("Connected!!")
+      client.subscribe('/topic/messages', function (msg) {
+        currentComponent.setState({ messages: msg.body })
+      });
+    }
+
+    let onDisconnected = () => {
+      console.log("Disconnected!!")
+    }
+
+    let client = new Client({
+      brokerURL: SOCKET_URL,
+      reconnectDelay: 5000,
+      heartbeatIncoming: 4000,
+      heartbeatOutgoing: 4000,
+      onConnect: onConnected,
+      onDisconnect: onDisconnected
+    });
+
+    client.activate();
+
+    setTimeout(() => {
+      client.publish({
+        destination: '/app/send',
+        body: {
+          from: "1",
+          text: "Text"
+        },
+        headers: { priority: '9', 'content-type': 'application/json' },
+      });
+    }, 2000)
+
+  };
+
+  render() {
+    return (
+      <div>
+        <div>{this.state.messages}</div>
+      </div>
+    );
+  }
+
 }
 
-export default App;
+export default App_Stomp;
